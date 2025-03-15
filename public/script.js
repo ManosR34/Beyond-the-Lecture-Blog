906fdc4ba7451af61bfe17076b9c0c681d54fda9
Manos <manos@mac.home>
2025-03-15 13:47:34 +0100

    New Header and syle layout

--- public/script.js
+++ public/script.js
@@ -1,3 +1,16 @@
 function toggleMenu() {
     document.querySelector(".headerContainer__navLinks").classList.toggle("active");
 }
+
+const navBar = document.getElementById('navBar');
+const overlay = document.getElementById('overlay');
+
+function openSideBar() {
+    navBar.classList.add('show');
+    overlay.classList.add('show');
+}
+
+function closeSideBar() {
+    navBar.classList.remove('show');
+    overlay.classList.remove('show');
+}
